0dc172891e23a0ac903fd39cdb380ddf5cb12cb4
lala <13687739+daedalus1215@users.noreply.github.com>
2024-10-13 21:12:20 -0400

    wire up mongoose module in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './domain/users.service';
 import { UsersController } from './app/controllers/users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './infra/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
