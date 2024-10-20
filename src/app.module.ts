9ae2c7070f35a5cc7d1605116de6eafe0cf36583
lala <13687739+daedalus1215@users.noreply.github.com>
2024-10-20 16:20:57 -0400

    setup mongo correctly in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './domain/app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,20 +14,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        return process.env.NODE_ENV === "development"
-          ? {
-              uri: `mongodb://${configService.get<string>("DATABASE_HOST")}:${configService.get<string>("DATABASE_PORT")}/${configService.get<string>("DATABASE_NAME")}`,
-            }
-          : {
-              uri: `mongodb://${configService.get<string>("DATABASE_USER")}:${configService.get<string>("DATABASE_PASSWORD")}@${configService.get<string>("DATABASE_HOST")}:${configService.get<string>("DATABASE_PORT")}/${configService.get<string>("DATABASE_NAME")}`,
-            };
-      },
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb:`
+          + `//${configService.get<string>("DATABASE_HOST")}`
+          + `:${configService.get<string>("DATABASE_PORT")}`
+          + `/${configService.get<string>("DATABASE_NAME")}`,
+      }),
       inject: [ConfigService],
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
