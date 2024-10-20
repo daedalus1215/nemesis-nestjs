b058f59d475379056c4791e80f428da4be3c2e88
lala <13687739+daedalus1215@users.noreply.github.com>
2024-10-20 16:28:39 -0400

    Wire up jwt authorization on profile

--- src/users/app/controllers/users.controller.ts
+++ src/users/app/controllers/users.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from 'src/users/domain/users.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
-    @Post('register')
-    @HttpCode(HttpStatus.CREATED)
-    async register(@Body() createUserDto: CreateUserDto) {
-      const user = await this.usersService.createUser(createUserDto);
-      return user;
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req) {
+    return req.user;
+  }
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.createUser(createUserDto);
+    return user;
+  }
 
 }
