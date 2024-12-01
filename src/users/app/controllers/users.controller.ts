9164ca902389ff49a68d1c2190761a6924eaf076
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-12-01 08:43:53 -0500

    add-swagger: Add swagger notation to controllers

--- src/users/app/controllers/users.controller.ts
+++ src/users/app/controllers/users.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { 
+  Body, 
+  Controller, 
+  Get, 
+  HttpCode, 
+  HttpStatus, 
+  Post, 
+  Req, 
+  UseGuards 
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from 'src/users/domain/services/users.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/app/jwt-auth.guard';
 
+@ApiTags('users') // Tagging the controller for Swagger
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth() // Indicates this route requires authentication
+  @ApiOperation({ summary: 'Get user profile' }) // Description for the endpoint
+  @ApiResponse({ status: 200, description: 'User profile retrieved successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Get('profile')
   getProfile(@Req() req) {
     return req.user;
@@ -15,6 +30,10 @@ export class UsersController {
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Register a new user' }) // Description for the endpoint
+  @ApiBody({ type: CreateUserDto }) // Documenting the request body
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
   async register(@Body() createUserDto: CreateUserDto) {
     const user = await this.usersService.createUser(createUserDto);
     return user;
@@ -22,6 +41,9 @@ export class UsersController {
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get list of all users' }) // Description for the endpoint
+  @ApiResponse({ status: 200, description: 'List of users retrieved successfully' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async getUsers(): Promise<any> {
     return await this.usersService.getUsers();
   }
