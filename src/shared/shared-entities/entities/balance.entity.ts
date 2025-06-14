764ccc01a557afc3fd4461461563deb5601e854a
la la <13687739+daedalus1215@users.noreply.github.com>
2025-06-13 20:48:03 -0400

    setup: move entities to a shared domain like concept

---
+++ src/shared/shared-entities/entities/balance.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+import { Transaction } from './transaction.entity';
+
+@Entity()
+export class Balance {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('decimal', { precision: 20, scale: 8, default: 0 })
+  amount: number;
+
+  @ManyToOne(() => User, { nullable: false })
+  owner: User;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.fromBalance)
+  outgoingTransactions: Transaction[];
+
+  @OneToMany(() => Transaction, (transaction) => transaction.toBalance)
+  incomingTransactions: Transaction[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
---
+++ src/shared/shared-entities/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 20 })
+  username: string;
+
+  @Column({ length: 100 })
+  password: string;
+}
