9f4f96c2f197fe1e34155a0e2061394f04ca9b87
la la <13687739+daedalus1215@users.noreply.github.com>
2025-06-14 07:37:40 -0400

    create-user: wire up the modules

--- src/ledger/ledger.module.ts
+++ src/ledger/ledger.module.ts
@@ -8,21 +8,31 @@ import { TransferAction } from './balance/app/controllers/transfer-action/transf
 import { BalanceRepository } from './balance/infrastructure/repositories/balance.repository';
 import { TransactionRepository } from './balance/infrastructure/repositories/transaction.repository';
 import { Transaction } from 'src/shared/shared-entities/entities/transaction.entity';
+import { GetBalanceHelper } from './balance/domain/transaction-scripts/transaction-scripts-facade/get-balance.helper';
+import { TransactionScriptFacade } from './balance/domain/transaction-scripts/transaction-scripts-facade/transaction.script.facade';
+import { GetAllTransactionsForUserTS } from './balance/domain/transaction-scripts/get-all-transactions-for-user-TS/get-all-transactions-for-user.transaction.script';
+import { TransferTS } from './balance/domain/transaction-scripts/transfer-TS/transfer.transaction.script';
+import { TransferConverter } from './balance/app/controllers/transfer-action/transfer.converter';
+import { BalanceAggregator } from './balance/domain/aggregators/balance.aggregator';
+import { GetBalanceTS } from './balance/domain/transaction-scripts/get-balance-TS/get-balance.transaction.script';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Balance, Transaction, Invoice])],
   controllers: [GetBalanceAction, TransferAction],
   providers: [
     BalanceService,
-    {
-      provide: 'BalanceRepositoryPort',
-      useClass: BalanceRepository,
-    },
-    {
-      provide: 'TransactionRepositoryPort',
-      useClass: TransactionRepository,
-    },
+    BalanceRepository,
+    { provide: 'BalanceRepositoryPort', useClass: BalanceRepository },
+    TransactionRepository,
+    { provide: 'TransactionRepositoryPort', useClass: TransactionRepository },
+    TransactionScriptFacade,
+    GetBalanceHelper,
+    GetAllTransactionsForUserTS,
+    TransferTS,
+    TransferConverter,
+    BalanceAggregator,
+    GetBalanceTS,
   ],
-  exports: [BalanceService],
+  exports: [BalanceService, BalanceAggregator, GetBalanceTS],
 })
 export class LedgerModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,15 +9,23 @@ import { UserRepository } from './infrastructure/user.repository';
 import { GetProfileConverter } from './app/controllers/get-profile-action/get-profile.converter';
 import { GetUserConverter } from './app/controllers/get-user-action/get-user.converter';
 import { RegisterUserConverter } from './app/controllers/register-user-action/register-user.converter';
+import { CreateUserTransactionScript } from './domain/transaction-scripts/create-user-ts/create-user.transaction.script';
+import { UserExistsValidator } from './domain/transaction-scripts/create-user-ts/validators/user-exists.validatgor';
+import { IsPasswordStrongValidator } from './domain/transaction-scripts/create-user-ts/validators/is-password-strong.validator';
+import { ConfigModule } from '@nestjs/config';
+import { LedgerModule } from 'src/ledger/ledger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule, LedgerModule],
   providers: [
     UsersService,
     UserRepository,
     GetProfileConverter,
     GetUserConverter,
     RegisterUserConverter,
+    CreateUserTransactionScript,
+    UserExistsValidator,
+    IsPasswordStrongValidator,
   ],
   controllers: [GetProfileAction, GetUserAction, RegisterUserAction],
   exports: [UsersService, UserRepository],
