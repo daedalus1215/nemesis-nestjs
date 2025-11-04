5274a2a6458172a3f3978c85ddc1ac7f5847006f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2025-11-03 22:05:13 -0500

    payments: update fetch user accounts use-case. Return the accounts balance

--- src/ledger/accounts/app/actions/fetch-user-accounts-action/fetch-user-accounts.action.ts
+++ src/ledger/accounts/app/actions/fetch-user-accounts-action/fetch-user-accounts.action.ts
@@ -4,13 +4,17 @@ import {
   GetAuthUser,
 } from '../../../../../auth/app/decorators/get-auth-user.decorator';
 import { ProtectedAction } from '../../../../../shared/application/protected-action-options';
-import { AccountAggregator } from '../../../domain/aggregators/account.aggregator';
 import { FetchUserAccountsResponseDto } from './fetch-user-accounts.response.dto';
 import { FetchUserAccountsSwagger } from './fetch-user-accounts.swagger';
+import { FetchUserAccountsResponder } from './fetch-user-accounts.responder';
+import { AccountBalanceService } from 'src/ledger/accounts/domain/services/account-balance.service';
 
 @Controller('accounts')
 export class GetUserAccountsAction {
-  constructor(private readonly accountAggregator: AccountAggregator) {}
+  constructor(
+    private readonly accountBalanceService: AccountBalanceService,
+    private readonly responder: FetchUserAccountsResponder,
+  ) {}
 
   @Get()
   @ProtectedAction({
@@ -21,18 +25,8 @@ export class GetUserAccountsAction {
   async handle(
     @GetAuthUser() user: AuthUser,
   ): Promise<FetchUserAccountsResponseDto> {
-    const accounts = await this.accountAggregator.getUserAccounts(user.userId);
-
-    //@TODO: Move to a Responder.
-    return {
-      accounts: accounts.map((account) => ({
-        id: account.id,
-        name: account.name,
-        accountType: account.accountType,
-        isDefault: account.isDefault,
-        createdAt: account.createdAt,
-      })),
-      success: true,
-    };
+    return this.responder.apply(
+      await this.accountBalanceService.getAccountsWithBalances(user.userId),
+    );
   }
 }
