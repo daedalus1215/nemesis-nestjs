4cc950b787878e78d3d85bc310336735c84a3d4c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-01-03 20:39:34 -0500

    refactor(auth): format useFactory function for better readability

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,9 @@ import { UsersModule } from 'src/users/users.module';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => {
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<JwtModuleOptions> => {
         const expiresIn = configService.get<string>('JWT_EXPIRES_IN', '1m');
         return {
           secret: configService.get<string>('JWT_SECRET'),
